ecbacc1659a56987c4334abccc768eefdf065656
eatpoopandgrowstrong <CTJH22.19@ICHAT.SP.EDU.SG>
2021-01-24 21:24:52 +0800

    datasheet content and images

---
+++ hub/WORKFILESFORMULTIPLEPC/Arduino/SoftwareSerialTesting/SoftwareSerialTesting.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial myserial(10,11)
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+
+}
