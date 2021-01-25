d0ad25a096cf3c73c54e45343c726ff1014edc49
eatpoopandgrowstrong <CTJH22.19@ICHAT.SP.EDU.SG>
2021-01-25 10:41:10 +0800

    nav bar fix and content

--- hub/WORKFILESFORMULTIPLEPC/Arduino/SoftwareSerialTesting/SoftwareSerialTesting.ino
+++ hub/WORKFILESFORMULTIPLEPC/Arduino/SoftwareSerialTesting/SoftwareSerialTesting.ino
@@ -1,18 +1,19 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial myserial(10,11)
+SoftwareSerial MySerial(10,11)
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  
+  if(MySerial.available()){
+    Serial.write(MySerial.read();
+  }
 
 }
