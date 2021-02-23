bfcaa1e56d51e8b977424d78279aa860c9960ba7
eatpoopandgrowstrong <CTJH22.19@ICHAT.SP.EDU.SG>
2021-02-23 23:06:40 +0800

    content

--- hub/MODULEPROJECT/Code/sketch_feb18a/sketch_feb18a.ino
+++ hub/MODULEPROJECT/Code/sketch_feb18a/sketch_feb18a.ino
@@ -1,30 +1,56 @@
-const int LEDTEST = 0;
-const int DCMOTOR = 9;
+//#include <IRremote.h>
+#include <Servo.h>
 
+const int LEDTEST = 0;
+const int DCMOTOR1 = 9;
+const int DCMOTOR2 = 8;
+
+
+const int IRpin = 7;
+
+int Button1 = 0;
+
+Servo Servo1;
 
 void setup() {
   // put your setup code here, to run once:
 
   pinMode(LEDTEST,OUTPUT);
   pinMode(DCMOTOR,OUTPUT);
+pinMode(DCMOTOR1,OUTPUT);
+  Serial.begin(9600);
+  //IrReceiver.begin(IRpin, ENABLE_LED_FEEDBACK); // Start the receiver, enable feed
+
+  
+  Servo1.attach(10);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(DCMOTOR,HIGH);
+  Servo1.write(0);
 
-  digitalWrite(LEDTEST,HIGH);
+  delay(1000);
 
-  delay(500);
+  Servo1.write(90);
 
-  digitalWrite(LEDTEST,LOW);
-
-  delay(500);
+  delay(1000);
 
   
 
+}
 
 
+
+
+void Lights(){
+
+  if(Button1 == 1){
+
+    digitalWrite(LEDTEST ,HIGH);
+
+    
+  }
+  
 }
